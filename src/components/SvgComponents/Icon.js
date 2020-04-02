4d855135ad68614c7a21dc39051f0cc59dd5a425
Charles Gael DJEDAI <charlesgael.djedai@outlook.com>
2020-04-02 18:19:53 +0200

    App icon

---
+++ src/components/SvgComponents/Icon.js
@@ -0,0 +1,95 @@
+import * as React from "react";
+
+function SvgIcon(props) {
+    return (
+        <svg width="112mm" height="112mm" viewBox="0 0 112 112" {...props}>
+            <g transform="translate(1 11)">
+                <rect width={100} height={68} ry={4.676} fill="#f8f3f3" />
+                <rect x={0.5} y={68} width={99} height={11} ry={0} fill="#84dbff" paintOrder="markers fill stroke" />
+                <rect width={100} height={80} ry={4.676} fill="none" stroke="#00a0df" strokeWidth={2} />
+                <rect
+                    x={0.5}
+                    y={68}
+                    width={99}
+                    height={0.212}
+                    ry={0}
+                    fill="none"
+                    stroke="#00a0df"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1.567}
+                    paintOrder="markers fill stroke"
+                />
+                <rect
+                    x={6}
+                    y={72}
+                    width={88}
+                    height={3.742}
+                    ry={1.804}
+                    fill="#f9dd9d"
+                    stroke="#00a0df"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    paintOrder="markers fill stroke"
+                />
+                <g transform="translate(10 10)">
+                    <rect width={100} height={68} ry={4.676} fill="#f8f3f3" />
+                    <rect x={0.5} y={68} width={99} height={11} ry={0} fill="#84dbff" paintOrder="markers fill stroke" />
+                    <rect width={100} height={80} ry={4.676} fill="none" stroke="#00a0df" strokeWidth={2} />
+                    <rect
+                        x={0.5}
+                        y={68}
+                        width={99}
+                        height={0.212}
+                        ry={0}
+                        fill="none"
+                        stroke="#00a0df"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.567}
+                        paintOrder="markers fill stroke"
+                    />
+                    <rect
+                        x={6}
+                        y={72}
+                        width={88}
+                        height={3.742}
+                        ry={1.804}
+                        fill="#f9dd9d"
+                        stroke="#00a0df"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        paintOrder="markers fill stroke"
+                    />
+                    <circle
+                        cx={16}
+                        cy={74}
+                        r={3.675}
+                        fill="#fff"
+                        stroke="#00a0df"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        paintOrder="markers fill stroke"
+                    />
+                    <circle
+                        cx={50}
+                        cy={34}
+                        r={20}
+                        fill="#f9a287"
+                        stroke="#00a0df"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        paintOrder="markers fill stroke"
+                    />
+                    <path d="M46 29l10 6-10 6z" fill="#f8f3f3" stroke="#00a0df" strokeLinejoin="round" strokeWidth={2} />
+                </g>
+            </g>
+        </svg>
+    );
+}
+
+export default SvgIcon;
